44185053f4824f7888cf0177c63aefb54cafeb5f
emailjkj <emailjkj@gmail.com>
2021-09-24 19:58:58 +0400

    Config Server

---
+++ central-config/src/main/java/com/etisalat/centralconfig/CentralConfigApplication.java
@@ -0,0 +1,13 @@
+package com.etisalat.centralconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralConfigApplication.class, args);
+	}
+
+}
